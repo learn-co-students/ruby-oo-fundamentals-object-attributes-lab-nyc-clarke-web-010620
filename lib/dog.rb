f317b671491e641885c9e945be11ece6d41f0ec9
Leon Jamison <leonjamison@icloud.com>
2020-01-07 17:38:38 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+    # def initialize(given_name, given_breed)
+    # @name = given_name
+    # @breed = given_breed
+    # end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(given_name)
+        @name = given_name
+    end 
+
+    def breed=(given_breed)
+        @breed = given_breed
+    end 
+end 
+
+
+
