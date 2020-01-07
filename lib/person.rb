329f0859682a041d1c9df49fec634a555663664b
kgc35 <kntchn@gmail.com>
2020-01-07 17:30:47 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+   # def initialize(given_name, given_job)
+    #    @name = given_name
+     #   @job = given_job
+    #end
+
+    def name
+        @name
+    end
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(new_job)
+        @job = new_job
+    end
+end
+
+
+
+        
